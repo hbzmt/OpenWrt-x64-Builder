e30f7a7c6ed937781a2fdcc17d18bf001682edc8
hbzmt <58494002+hbzmt@users.noreply.github.com>
2026-06-27 12:51:37 +0800

    Create custom-packages.sh

---
+++ shell/custom-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CUSTOM_PACKAGES=""
+
+# =========================
+# OpenClash（你现在要做的）
+# =========================
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
+
+# =========================
+# 你可以继续加
+# =========================
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-adguardhome"
+
+echo "$CUSTOM_PACKAGES"
