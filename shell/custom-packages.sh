b57fdb3ede679d532d4813a2a619245f98fe22ec
hbzmt <58494002+hbzmt@users.noreply.github.com>
2026-06-27 13:35:17 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -5,7 +5,7 @@ CUSTOM_PACKAGES=""
 # =========================
 # OpenClash（你现在要做的）
 # =========================
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash luci-compat kmod-tun kmod-inet-diag kmod-nft-tproxy bash curl ip-full unzip"
 
 # =========================
 # 你可以继续加
